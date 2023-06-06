6aef8df047c1ddd7bcddc5053e74d5e3fea384be
Andrey Gorbachev <andreygorbachev@hotmail.com>
2023-06-06 10:24:33 +0100

    Include guard.

--- test/setup.h
+++ test/setup.h
@@ -20,6 +20,8 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 // SOFTWARE.
 
+#pragma once
+
 #include <annual_holidays.h>
 #include <weekend.h>
 #include <schedule.h>
